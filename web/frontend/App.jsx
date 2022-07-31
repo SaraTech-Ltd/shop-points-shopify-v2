a94eb21dc87b26976f3752d344976a0a64f1b405
sahidhossen <sahidhossenbd@gmail.com>
2022-08-01 00:43:33 +0600

    Update vite config

--- web/frontend/App.jsx
+++ web/frontend/App.jsx
@@ -4,23 +4,23 @@ import Routes from './Routes';
 import store from './store';
 
 import { AppBridgeProvider, GraphQLProvider, PolarisProvider } from './components';
-
+console.log('env:', import.meta.env);
 export default function App() {
-	// Any .tsx or .jsx files in /pages will become a route
-	// See documentation for <Routes /> for more info
-	const pages = import.meta.globEager('./pages/**/!(*.test.[jt]sx)*.([jt]sx)');
+  // Any .tsx or .jsx files in /pages will become a route
+  // See documentation for <Routes /> for more info
+  const pages = import.meta.globEager('./pages/**/!(*.test.[jt]sx)*.([jt]sx)');
 
-	return (
-		<Provider store={store}>
-			<BrowserRouter>
-				<PolarisProvider>
-					<AppBridgeProvider>
-						<GraphQLProvider>
-							<Routes pages={pages} />
-						</GraphQLProvider>
-					</AppBridgeProvider>
-				</PolarisProvider>
-			</BrowserRouter>
-		</Provider>
-	);
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <PolarisProvider>
+          <AppBridgeProvider>
+            <GraphQLProvider>
+              <Routes pages={pages} />
+            </GraphQLProvider>
+          </AppBridgeProvider>
+        </PolarisProvider>
+      </BrowserRouter>
+    </Provider>
+  );
 }
